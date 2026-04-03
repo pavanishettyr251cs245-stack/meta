e12a08b2d7be9737df3c3731f6661f514cdad013
Pavani Shetty <pavanishettyr.251cs245@nitk.edu.in>
2026-04-03 12:53:11 +0530

    Remove openenv dependency from environment file

--- server/my_env2_environment.py
+++ server/my_env2_environment.py
@@ -1,30 +1,24 @@
 from uuid import uuid4
-from openenv.core.env_server.types import State
 from environment.core import InsuranceClaimEnvironment
-from environment.schemas import ClaimAction, ClaimObservation
-
-try:
-    from ..models import MyEnv2Action, MyEnv2Observation
-except ImportError:
-    from models import MyEnv2Action, MyEnv2Observation
 
 class MyEnv2Environment:
     SUPPORTS_CONCURRENT_SESSIONS: bool = True
 
     def __init__(self):
         self._env = InsuranceClaimEnvironment({"max_steps": 6})
-        self._state = State(episode_id=str(uuid4()), step_count=0)
+        self._episode_id = str(uuid4())
+        self._step_count = 0
 
     def reset(self):
-        self._state = State(episode_id=str(uuid4()), step_count=0)
-        obs = self._env.reset()
-        return obs
+        self._episode_id = str(uuid4())
+        self._step_count = 0
+        return self._env.reset()
 
     def step(self, action):
-        self._state.step_count += 1
+        self._step_count += 1
         obs, reward, done, info = self._env.step(action)
         return obs
 
     @property
     def state(self):
-        return self._state
+        return {"episode_id": self._episode_id, "step_count": self._step_count}
